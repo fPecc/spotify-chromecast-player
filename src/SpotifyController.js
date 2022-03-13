3846c13fca00bd0ec2458f34248ebbdcd30f432e
Federico Peccia <f.d.peccia@gmail.com>
2022-03-13 11:06:57 +0100

    Pending commit

--- src/SpotifyController.js
+++ src/SpotifyController.js
@@ -22,38 +22,31 @@ SpotifyController.prototype.authenticate = function ({ username, password, devic
 
 		that.device_name = device_name;
 
-		SpotifyWPAT.getAccessToken(username, password).then(function (access_token) {
-			that.access_token = access_token;
-			global_access_token = access_token;
+		// Send setCredentials request using web AT
+		console.log('sending getInfo...');
+		that.send({
+			type: 'getInfo',
+			payload: {}
+		});
 
-			that.api = new SpotifyWebApi({
-				accessToken: that.access_token
-			});
-
-			// Send setCredentials request using web AT
-			console.log('sending getInfo...');
-			that.send({
-				type: 'getInfo',
-				payload: {}
-			});
-
-			// Once Chromecast replies, get the list of the devices
-			that.on('message', function (message) {
-				console.log(message);
-				if(message.type === 'getInfoResponse')
-				{
+		// Once Chromecast replies, get the list of the devices
+		that.on('message', function (message) {
+			console.log(message);
+			if(message.type === 'getInfoResponse')
+			{
+				SpotifyWPAT.getAccessToken(username, password).then(function (access_token) {
 					var device = message["payload"]["deviceID"]
-            		var client = message["payload"]["clientID"]
+					var client = message["payload"]["clientID"]
 					headers = {
-                		'authority': 'spclient.wg.spotify.com',
-                		'authorization': 'Bearer ' + global_access_token,
-                		'content-type': 'text/plain;charset=UTF-8'
-            		}
-					console.log(global_access_token)
+						'authority': 'spclient.wg.spotify.com',
+						'authorization': 'Bearer ' + access_token,
+						'content-type': 'text/plain;charset=UTF-8'
+					}
+					console.log(access_token)
 
-            		request_body = JSON.stringify({'clientId': client, 'deviceId': device})
+					request_body = JSON.stringify({'clientId': client, 'deviceId': device})
 
-            		request({
+					request({
 						uri:'https://spclient.wg.spotify.com/device-auth/v1/refresh', 
 						method: 'POST',
 						headers: headers, 
@@ -72,19 +65,36 @@ SpotifyController.prototype.authenticate = function ({ username, password, devic
 						})
 
 					})
-				}
-				if (message.type === 'setCredentialsResponse') {
+				});
+			}
+			if (message.type === 'setCredentialsResponse') {
 
-					that.api.getMyDevices().then(function (response) {
-						var devices = response.body.devices;
-						var device = devices.find(e => e.name === that.device_name);
-						that.device = device;
+				that.api.getMyDevices().then(function (response) {
+					var devices = response.body.devices;
+					var device = devices.find(e => e.name === that.device_name);
+					that.device = device;
 
-						resolve(that);
-					})
-				}
+					resolve(that);
+				})
+			}
+		});
+
+		/*SpotifyWPAT.getAccessToken(username, password).then(function (access_token) {
+			that.access_token = access_token;
+			global_access_token = access_token;
+
+			that.api = new SpotifyWebApi({
+				accessToken: that.access_token
 			});
-		})
+
+			// Send setCredentials request using web AT
+			console.log('sending getInfo...');
+			that.send({
+				type: 'getInfo',
+				payload: {}
+			});
+
+		})*/
 
 	});
 };
